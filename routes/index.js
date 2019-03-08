e0c20ab6c8f41bac35668ad3a91e4b68db1193c6
Jeff <jffryteo@hotmail.com>
2019-03-08 14:16:51 +0800

    edited index routes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 module.exports = function(app){
 
 
-  const {getEvent1,getHomeUser,getHomePage,getLogin,getVouchers,getCreateFromScratch,getCreateFromVoucher,getJoin,getUserAcc,registerUser,getUserProfile} = require('../controllers/createEventController.js');
+  const {getEvent3,getEvent2,getEvent1,getHomeUser,getHomePage,getLogin,getVouchers,getCreateFromScratch,getCreateFromVoucher,getJoin,getUserAcc,registerUser,getUserProfile} = require('../controllers/createEventController.js');
 
 
 // routing
@@ -17,4 +17,6 @@ app.get('/userAcc',getUserAcc);
 app.get('/userProfile',getUserProfile);
 app.get('/homeUser',getHomeUser);
 app.get('/event1',getEvent1);
+app.get('/event2',getEvent2);
+app.get('/event3',getEvent3);
 }
